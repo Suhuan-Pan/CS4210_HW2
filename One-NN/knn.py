3a74c46f2c1e81a7fe4752ecf286dd52aff5083a
Suhuan Pan <span@cpp.edu>
2023-10-05 07:50:01 -0700

    Add files via upload

--- One-NN/knn.py
+++ One-NN/knn.py
@@ -1,4 +1,13 @@
-
+# -------------------------------------------------------------------------
+# AUTHOR: Suhuan Pan
+# FILENAME: title of the source file: knn.py
+# SPECIFICATION: use python to read a csv file and plot a decision tree
+# FOR: CS 4210 - Assignment #2
+# TIME SPENT: 2 hours
+# -----------------------------------------------------------*/
+# IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH
+# AS numpy OR pandas. You have to work here only with standard
+# dictionaries, lists, and arrays
 #-----------------------------------------------------------*/
 # IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH
 # AS numpy OR pandas. You have to work here only with standard vectors and arrays
@@ -6,11 +15,12 @@
 # importing some Python libraries
 
 import csv
-from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 
 db = []
+X = []
+Y = []
 
 # reading the data in a csv file
 with open('binary_points.csv', 'r') as csvfile:
@@ -18,88 +28,61 @@ with open('binary_points.csv', 'r') as csvfile:
     for i, row in enumerate(reader):
         if i > 0: #skipping the header
             db.append (row)
-            # print(row)
 
-
+# store data set
 currentRow = []
-# loop your data to allow each instance to be your test set
 for i in range(len(db)):
     currentRow.append([])
     for ele in db[i]:
         currentRow[i].append(ele)
 
+# print("stored data set row by row: ", currentRow)
 
-X = []
-Y = []
-
-x_test = []
-y_test = []
-
+# split to data and class label set
+xx = []
+yy = []
 for row in currentRow:
     data = row[0:2]
     class_label = row[2]
+    xx.append(data)
+    yy.append(class_label)
 
-    x_test.append(data)
-    y_test.append(class_label)
+# print("After split data set and class label set: ")
+# print(xx)
+# print(yy)
+
+# convert each elements in nested list into float type
+# also store the test sample of this iteration in the vector testSample
+fx = []
+testSample = []
+for row in xx:
+    fx = [float(ele) for ele in row]
+    X.append(fx)
+    testSample.append(fx)
+
+# convert positive to class = 1, negative to class = 2
+fy = []
+for j in range(len(yy)):
+    if yy[j] == '+': fy = float (2.0)
+    else: fy = float(1.0)
+    Y.append(fy)
+
+print("After transfer all data to numerical value: ")
+print(X)
+print(Y)
+print(testSample)
 
 
-f = []
-sample = []
-for row in x_test:
-    f = [float(ele) for ele in row]
-    sample.append(f)
-
-
-print(sample)
-print(y_test)
-
-
+# fitting the knn with the training data
 clf = KNeighborsClassifier(n_neighbors=1, p=2)
-clf = clf.fit(sample, y_test)
-
-y_predict = clf.predict(sample)
-N = 0;
-for i in range (len(y_predict)):
-    for ele in y_test:
-        if ele != y_predict[i]:
-            N = N + 1
-
-
-print (N)
-
-
-
-
-
-
-# add the training features to the 2D array X removing the instance that will be
-# used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]].
-# Convert each feature value to
-# float to avoid warning messages
-# --> add your Python code here
-
-
-# transform the original training classes to numbers and add to the vector Y
-# removing the instance that will be used for testing in this iteration. For
-# instance, Y = [1, 2, ,...]. Convert each
-# feature value to float to avoid warning messages
-# --> add your Python code here
-# Y =
-
-# store the test sample of this iteration in the vector testSample
-# --> add your Python code here
-# testSample = KNearestNeighbors
-
-# fitting the knn to the data
-# clf = KNeighborsClassifier(n_neighbors=1, p=2)
-# clf = clf.fit(X, Y)
+clf.fit(testSample, Y)
 
 # use your test sample in this iteration to make the class prediction. For instance:
-# class_predicted = clf.predict([[1, 2]])[0]
-# --> add your Python code here
-# compare the prediction with the true label of the test instance
-# to start calculating the error rate.
-# --> add your Python code here
-# print the error rate
-# --> add your Python code here
-# --> add your Python code here
+# class_predicted = clf.predict([[1, 2]])[0]s
+# convert to list of float numbers
+final_predict = []
+predict = clf.predict(testSample)
+for r in predict:
+    final_predict.append(r)
+
+# print("predicted class label: ", final_predict)
