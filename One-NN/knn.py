735f0b54464ccd82ca708d48a5c85da246262c92
Suhuan Pan <span@cpp.edu>
2023-10-06 20:12:46 -0700

    Add files via upload

--- One-NN/knn.py
+++ One-NN/knn.py
@@ -1,9 +1,9 @@
 # -------------------------------------------------------------------------
 # AUTHOR: Suhuan Pan
 # FILENAME: title of the source file: knn.py
-# SPECIFICATION: use python to read a csv file and plot a decision tree
+# SPECIFICATION: use python to read a csv file and predict 1NN with leave one out cross validation
 # FOR: CS 4210 - Assignment #2
-# TIME SPENT: 2 hours
+# TIME SPENT: feeling shame to say it because i spent lots of time but can't get the correct result
 # -----------------------------------------------------------*/
 # IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH
 # AS numpy OR pandas. You have to work here only with standard
@@ -30,59 +30,151 @@ with open('binary_points.csv', 'r') as csvfile:
             db.append (row)
 
 # store data set
-currentRow = []
+dataRow = []
 for i in range(len(db)):
-    currentRow.append([])
+    dataRow.append([])
     for ele in db[i]:
-        currentRow[i].append(ele)
-
+        dataRow[i].append(ele)
 # print("stored data set row by row: ", currentRow)
 
 # split to data and class label set
-xx = []
-yy = []
-for row in currentRow:
-    data = row[0:2]
-    class_label = row[2]
-    xx.append(data)
-    yy.append(class_label)
+data = []
+class_label = []
+for row in dataRow:
+    c1 = row[0:2]
+    c2 = row[2]
+    data.append(c1)
+    class_label.append(c2)
 
 # print("After split data set and class label set: ")
 # print(xx)
 # print(yy)
 
 # convert each elements in nested list into float type
-# also store the test sample of this iteration in the vector testSample
 fx = []
 testSample = []
-for row in xx:
+for row in data:
     fx = [float(ele) for ele in row]
     X.append(fx)
-    testSample.append(fx)
 
 # convert positive to class = 1, negative to class = 2
 fy = []
-for j in range(len(yy)):
-    if yy[j] == '+': fy = float (2.0)
-    else: fy = float(1.0)
+for j in range(len(class_label)):
+    if class_label[j] == '+': fy = float (1.0)
+    else: fy = float(2.0)
     Y.append(fy)
 
-print("After transfer all data to numerical value: ")
-print(X)
-print(Y)
-print(testSample)
+# print(X)
+# print(Y)
+
+counter = 0
+lastIndex = len(X)-1
+testSample_x = []
+testSample_y = []
 
 
-# fitting the knn with the training data
-clf = KNeighborsClassifier(n_neighbors=1, p=2)
-clf.fit(testSample, Y)
+for index in range(len(X)):
 
-# use your test sample in this iteration to make the class prediction. For instance:
-# class_predicted = clf.predict([[1, 2]])[0]s
-# convert to list of float numbers
-final_predict = []
-predict = clf.predict(testSample)
-for r in predict:
-    final_predict.append(r)
+    x_train = []
+    #y_train = []
+    test_x = []
+    test_y = []
 
-# print("predicted class label: ", final_predict)
+    # print("index = ", index)
+    test_x.append(X[index])
+    test_y.append(Y[index])
+    # print(test_x, test_y)
+
+    if index == 0:
+        x_train = X[index+1:len(X)]
+        y_train = Y[index+1:len(Y)]
+
+    elif index == lastIndex:
+        x_train = X[0:index]
+        y_train = Y[0:index]
+
+    else:
+        a = index+1
+        d = index-1
+
+        if d == 0:
+            # print("----- should be index 1 -----")
+            x_train = X[a:len(X)]
+            x_train.append(X[0])
+            y_train = Y[a:len(Y)]
+            y_train.append(Y[0])
+
+        elif a == lastIndex:
+            # print("----- should be index 8 -----")
+            x_train = X[0:index]
+            x_train.append(X[lastIndex])
+            y_train = Y[0:index]
+            y_train.append(Y[lastIndex])
+
+        else:
+           # print("a, b, c, d", a, lastIndex, 0, d)
+            x_train = X[a:len(X)]
+            x2 = X[0:d+1]
+            for row in x2:
+                x_train.append(row)
+            # print("x train: ", x_train)
+            # print("y train: ", y_train)
+
+
+    clf = KNeighborsClassifier(n_neighbors=1, p=2)
+    clf.fit(x_train, y_train)
+
+    class_predict = clf.predict(test_x)
+    # print("predict label, true label:", class_predict, test_y)
+    for ele in class_predict:
+        if ele != test_y:
+            counter = counter + 1
+            # print("unmatch: ", counter)
+
+
+
+print("Error rate: ", counter / len(Y))
+
+#print("train y: ", y_train)
+
+    # cnn = KNeighborsClassifier(n_neighbors=1, p=2)
+    # cnn = cnn.fit(x_train, y_train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
